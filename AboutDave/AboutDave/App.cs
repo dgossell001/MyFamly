3a0eca9f2172a6cf9b29500f1ec21dd3d051b944
dgossell001 <itpro@gossellmn.com>
2017-02-09 19:09:58 -0600

    Add project files.

---
+++ AboutDave/AboutDave/App.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AboutDave
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            string strTimeOfDay = "morning";
+            DateTime dtCurrent = DateTime.Now;
+            int intHour = dtCurrent.Hour;
+
+            if(11 < intHour & intHour < 17) { strTimeOfDay = "afternoon"; } // noon to 4:59 PM is afternoon
+            if ( 17 <= intHour & intHour <= 24) { strTimeOfDay = "evening"; } // 5PM to midnight is evening
+            if (intHour < 2) { strTimeOfDay = "evening"; }    // midnight to 1:59 is evening
+
+            string strMessage = "Good " + strTimeOfDay + ". My name is Dave(id).";
+            strMessage += "\nI am 6', 2\" tall and weigh a bit";
+            strMessage += " more than I'd like.";
+            strMessage += "\nI've been around long enough to remember when";
+            strMessage += " the \"shell\" WAS the user interface.";
+
+            var content = new ContentPage
+            {
+                Title = "AboutDave",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = strMessage
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MyFamily/MyFamily/FamilyPage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MyFamily
+{
+    public class FamilyPage : ContentPage
+    {
+        public FamilyPage()
+        {
+            FormattedString fstFamilyText = new FormattedString();
+
+            fstFamilyText.Spans.Add(new Span
+            {
+                Text = "\nParents\n",
+                FontAttributes = FontAttributes.Bold,
+                ForegroundColor = Color.Yellow
+            });
+
+            fstFamilyText.Spans.Add(new Span
+            {
+                Text = "Me (David) - Too many years to count.\nJoni - Information withheld by request.\n\n",
+                FontAttributes = FontAttributes.Italic,
+                ForegroundColor = Color.White
+            });
+
+            fstFamilyText.Spans.Add(new Span
+            {
+                Text = "Children\n",
+                FontAttributes = FontAttributes.Bold,
+                ForegroundColor = Color.Yellow
+            });
+
+            fstFamilyText.Spans.Add(new Span
+            {
+                Text = "Cory - 25 years\nJohn - 23 years\nHallie - 17 years\n\n",
+                FontAttributes = FontAttributes.Italic,
+                ForegroundColor = Color.White
+            });
+
+            fstFamilyText.Spans.Add(new Span
+            {
+                Text = "Grandson\n",
+                FontAttributes = FontAttributes.Bold,
+                ForegroundColor = Color.Yellow
+            });
+
+            fstFamilyText.Spans.Add(new Span
+            {
+                Text = "Tyrann - 11 months",
+                FontAttributes = FontAttributes.Italic,
+                ForegroundColor = Color.White
+            });
+
+            Label lblFamily = new Label();
+
+            lblFamily.FormattedText = fstFamilyText;
+            lblFamily.HorizontalOptions = LayoutOptions.Center;
+            lblFamily.FontFamily = "Times New Roman";
+
+            StackLayout stlLayout = new StackLayout();
+            stlLayout.Children.Add(lblFamily);
+            stlLayout.BackgroundColor = Color.Blue;
+
+            this.Content = stlLayout;
+        }
+    }
+}
