1980431f17456be0b9dc563ebbc403d6f379463d
dgossell001 <itpro@gossellmn.com>
2017-02-09 20:27:17 -0600

    Initial re-work for content logic in version 2.

--- MyFamily/MyFamily/FamilyPage.cs
+++ MyFamily/MyFamily/FamilyPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
@@ -12,61 +13,85 @@ namespace MyFamily
     {
         public FamilyPage()
         {
-            FormattedString fstFamilyText = new FormattedString();
+            StackLayout stlMainLayout = new StackLayout();
 
-            fstFamilyText.Spans.Add(new Span
+            string strName = "";
+            SortedDictionary<int, string> sdcNames = new SortedDictionary<int, string>();
+            string strRole = "";
+            SortedDictionary<int, string> sdcRoles = new SortedDictionary<int, string>();
+            Color clrFground = Color.Black;
+            SortedDictionary<int, Color> sdcFgrounds = new SortedDictionary<int, Color>();
+            Color clrBground = Color.White;
+            SortedDictionary<int, Color> sdcBgrounds = new SortedDictionary<int, Color>();
+
+            int intRelativeCount = 2;
+            
+            // Load some dictionaries with the relative's role, name, and display colors
+            for (int i=1; i<=intRelativeCount; i++)
             {
-                Text = "\nParents\n",
-                FontAttributes = FontAttributes.Bold,
-                ForegroundColor = Color.Yellow
-            });
+                switch (i)
+                {
+                    case 1:
+                        strName = "John Gossell, Jr.";
+                        strRole = "David's Paternal Grandfather";
+                        clrFground = Color.Red;
+                        clrBground = Color.White;
+                        break;
+                    case 2:
+                        strName = "Agnes Gossell";
+                        strRole = "David's Paternal Grandmother";
+                        clrFground = Color.Yellow;
+                        clrBground = Color.Black;
+                        break;
+                    default:
+                        break;
+                }
 
-            fstFamilyText.Spans.Add(new Span
+                sdcNames.Add(i, strName);
+                sdcRoles.Add(i, strRole);
+                sdcFgrounds.Add(i, clrFground);
+                sdcBgrounds.Add(i, clrBground);
+            }
+
+            // create formatted string for each relative's role and name,
+            //  then put the formatted sting into a label
+            for (int i = 1; i <+ intRelativeCount; i++)
             {
-                Text = "Me (David) - Too many years to count.\nJoni - Information withheld by request.\n\n",
-                FontAttributes = FontAttributes.Italic,
-                ForegroundColor = Color.White
-            });
+                FormattedString fstFamilyText = new FormattedString();
 
-            fstFamilyText.Spans.Add(new Span
-            {
-                Text = "Children\n",
-                FontAttributes = FontAttributes.Bold,
-                ForegroundColor = Color.Yellow
-            });
+                //Add a span for the relative's role
+                fstFamilyText.Spans.Add(new Span
+                {
+                    Text = sdcRoles[i],
+                    FontAttributes = FontAttributes.Bold,
+                    ForegroundColor = sdcFgrounds[i],
+                    BackgroundColor = sdcBgrounds[i]
+                });
 
-            fstFamilyText.Spans.Add(new Span
-            {
-                Text = "Cory - 25 years\nJohn - 23 years\nHallie - 17 years\n\n",
-                FontAttributes = FontAttributes.Italic,
-                ForegroundColor = Color.White
-            });
+                //Add a span for the relative's name
+                fstFamilyText.Spans.Add(new Span
+                {
+                    Text = sdcNames[i],
+                    FontAttributes = FontAttributes.Italic,
+                    ForegroundColor = sdcFgrounds[i],
+                    BackgroundColor = sdcBgrounds[i]
+                });
 
-            fstFamilyText.Spans.Add(new Span
-            {
-                Text = "Grandson\n",
-                FontAttributes = FontAttributes.Bold,
-                ForegroundColor = Color.Yellow
-            });
+                // Add a span as a spacer
+                fstFamilyText.Spans.Add(new Span
+                {
+                    Text = "SPACER",
+                    ForegroundColor = sdcBgrounds[i],
+                    BackgroundColor = sdcBgrounds[i]
+                });
 
-            fstFamilyText.Spans.Add(new Span
-            {
-                Text = "Tyrann - 11 months",
-                FontAttributes = FontAttributes.Italic,
-                ForegroundColor = Color.White
-            });
-
-            Label lblFamily = new Label();
-
-            lblFamily.FormattedText = fstFamilyText;
-            lblFamily.HorizontalOptions = LayoutOptions.Center;
-            lblFamily.FontFamily = "Times New Roman";
-
-            StackLayout stlLayout = new StackLayout();
-            stlLayout.Children.Add(lblFamily);
-            stlLayout.BackgroundColor = Color.Blue;
-
-            this.Content = stlLayout;
+                Label lblFamilyLabel = (new Label
+                {
+                    FormattedText = fstFamilyText
+                });
+            }
+            
+            this.Content = stlMainLayout;
         }
     }
 }
