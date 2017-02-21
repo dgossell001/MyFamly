441bb8fd33da4bb61ce547a014ef8cc606fa0ab6
dgossell001 <itpro@gossellmn.com>
2017-02-21 16:38:48 -0600

    Added ScrollView and StackLayout

--- MyFamily/MyFamily/App.cs
+++ MyFamily/MyFamily/App.cs
@@ -11,7 +11,7 @@ namespace MyFamily
     {
         public App()
         {
-            MainPage = new MyFamily();
+            MainPage = new xamlFaml();
         }
 
         protected override void OnStart()
--- TestApp/TestApp/MyXAMLPage.xaml.cs
+++ TestApp/TestApp/MyXAMLPage.xaml.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 
-namespace MyFamily
+namespace TestApp
 {
-    public partial class MyFamily : ContentPage
+    public partial class MyXAMLPage : ContentPage
     {
-        public MyFamily()
+        public MyXAMLPage()
         {
             InitializeComponent();
         }
